0a7ef1f0e368983d567c54cf70e8f411c912d91a
fantasiavsr <fantasiavsr@gmail.com>
2023-04-03 22:25:59 +0700

    finishing

--- code/spark-big-data/code4.py
+++ code/spark-big-data/code4.py
@@ -5,6 +5,9 @@ sc = SparkContext.getOrCreate();
 mylist = ["my", "pair", "rdd"]
 myRDD = sc.parallelize(mylist)
 myPairRDD = myRDD.map(lambda s: (s, len(s)))
-myPairRDD.collect()
-myPairRDD.keys().collect()
-myPairRDD.values().collect()
+# myPairRDD.collect()
+# myPairRDD.keys().collect()
+# myPairRDD.values().collect()
+print (myPairRDD.collect())
+print (myPairRDD.keys().collect())
+print (myPairRDD.values().collect())
